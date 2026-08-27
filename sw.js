0c6d0ca53496ecd5d9e1e714116003ab65ba4586
nguyentai219 <nguyentai219@gmail.com>
2026-08-27 14:32:09 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'khai-thue-v1.3.1';
+const CACHE = 'khai-thue-v1.3.5';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', function(e){
